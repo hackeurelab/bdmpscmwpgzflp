4d64043cbf2e980f55e64e139abbd3b66efafbd5
Vincent Carrubba <carrubba.vincent@gmail.com>
2016-05-29 21:13:57 +0200

    First working code on rpi

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
+
 import os
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN)
+
+def ButtonSmashed():
+    print "start playing sound"
+    os.system('mpg123 -q Connection-established-sound-effect.mp3 &')
+
+def ButtonUnsmashed():
+    print "stop playing sound"
+
+def HandleEdgeDetection(channel):
+    print GPIO.input(channel)
+    if GPIO.input(channel):
+        ButtonSmashed()
+    else:
+        ButtonUnsmashed()
 
 def main():
-    print('start play')
-    os.system('ffplay -autoexit -nodisp -v 0 assets/bonjour.mp3')
-    print('stop play')
+    GPIO.add_event_detect(18, GPIO.BOTH, callback=HandleEdgeDetection, bouncetime=200)
+    while True:
+        pass
     return
 
 if __name__ == '__main__':
